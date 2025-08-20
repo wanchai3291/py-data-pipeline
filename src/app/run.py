ffb268ac2aaa8a87bbc95da1b5d17908cc682915
wanchai3291 <wanchai.sirimat3291@gmail.com>
2025-08-20 13:38:46 +0700

    update code

--- src/app/run.py
+++ src/app/run.py
@@ -1,3 +1,27 @@
-def say_hello(name):
-    print(f"Hello, {name}!")
-    return f"Greeting sent to {name}"
+from dataclasses import dataclass
+from datetime import datetime, timezone
+from logging import Logger
+
+from app.constants import (
+    LOG_EXECUTION_COMPLETED,
+    LOG_EXECUTION_FAILED,
+    LOG_EXECUTION_STARTED,
+)
+
+def execute(
+    logger: Logger,
+    parameters,
+) -> None:
+    try:
+        start_timestamp = int(datetime.now(timezone.utc).timestamp() * 1000)
+
+
+        end_timestamp = int(datetime.now(timezone.utc).timestamp() * 1000)
+
+        logger.info(
+            f"{LOG_EXECUTION_COMPLETED} (elapsed: {end_timestamp - start_timestamp} ms)"
+        )
+    except Exception as e:
+        logger.info(LOG_EXECUTION_FAILED)
+        logger.error(e)
+        raise e
